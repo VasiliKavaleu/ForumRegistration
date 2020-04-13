74b726250d2197059a588a687b65d553e253fe7e
VasiliKavaleu <51245114+VasiliKavaleu@users.noreply.github.com>
2020-04-14 01:30:40 +0300

    Update models.py

--- forum/registration/models.py
+++ forum/registration/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Participants(models.Model):
     name = models.CharField(max_length=30, blank=False)
     surname = models.CharField(max_length=30, blank=False)
@@ -10,17 +9,10 @@ class Participants(models.Model):
     tel = models.IntegerField(blank=False)
     address = models.EmailField(blank=False)
 
-
-
 class Good(models.Model):
     theme = models.CharField(max_length=238, blank=True)
     theme_extended = models.CharField(max_length=238, blank=True)
     detail_description = models.TextField()
 
-    # name_good = models.CharField(max_length=30, unique=True, blank=True)
-    # data_add = models.DateField(auto_now=True)
-
     def __str__(self):
         return '{} {} {}'.format(self.detail_description, self.theme, self.theme_extended)
-
-
